feef8ae4a3317830e6c8ce032b8439922971a97b
abhay8696 <abhaykamble1346@gmail.com>
2023-12-09 20:23:50 +0530

    created 0s timeout to clear previous state of path before starting new run

--- src/components/square.js
+++ src/components/square.js
@@ -4,16 +4,19 @@ import { FollowedPathContext } from '../contexts/followedPathContext';
 import { findPath } from '../functions/findPath';
 
 import '../styles/board.css';
+import { useRef } from 'react';
 
 const Square = props => {
     const { x, y, standing_Vertical_Walls, standing_Horizontal_Walls, size } = props;
 
     const [followedPath, setFollowedPath] = useContext(FollowedPathContext);
+    const timeOutID = useRef(null);
 
-    const startRunning = (x,y)=>{
+    const startRunning = async (x,y)=>{
+        // alert("running")
         //clear previous path  
+        if(timeOutID) clearTimeout(timeOutID);
         setFollowedPath({path: new Set(), msg:null, visited: new Set(), crossedWalls: new Map()})
-
         // let visited = new Set();
         let { correctPath, msg, visited, crossedWalls } = findPath(x, y, new Set(), standing_Vertical_Walls, standing_Horizontal_Walls, size, new Map(), new Map());
         // if(!msg) console.log("failed. ratio: ", (visited.size/(size*size)*100));
@@ -21,7 +24,11 @@ const Square = props => {
         if(msg && !correctPath.has(`${x}-${y}`)) correctPath.set(`${x}-${y}`, correctPath.size+1);
         // console.log(correctPath)
 
-        setFollowedPath({path: correctPath, msg, visited, crossedWalls});
+        const timeID = setTimeout(() => {
+            setFollowedPath({path: correctPath, msg, visited, crossedWalls});
+        }, 0);
+
+        timeOutID.current = timeID;
     }
 
     const createSquare = ()=> {
